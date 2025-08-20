6c56ac23fe0851af3f86a62b798887bb4a70df3c
lirendada <2916776007@qq.com>
2025-08-20 17:30:08 +0800

    完成了消息队列快速入手的代码

---
+++ 25_8_17_rabbitmq_introduction/src/main/java/com/liren/consumer.java
@@ -0,0 +1,42 @@
+package com.liren;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class consumer {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 1. 创建连接工厂（同生产者）
+        ConnectionFactory factory = new ConnectionFactory();
+
+        // 2. 设置参数（同生产者）
+        factory.setHost("113.45.137.183");   // 设置ip
+        factory.setPort(5672);               // 设置端口
+        factory.setVirtualHost("lirendada"); // 设置虚拟主机名称
+        factory.setUsername("admin");        // 设置用户名
+        factory.setPassword("123123");       // 设置密码
+
+        // 3. 创建连接Connection（同生产者）
+        Connection connection = factory.newConnection();
+
+        // 4. 通过连接Connection创建通道Channel（同生产者）
+        Channel channel = connection.createChannel();
+
+        // 5. 声明一个队列（同生产者）（这是安全性措施，因为如果生产者还没创建队列的话，消费者这边直接读取会报错）
+        channel.queueDeclare("test", true, false, false, null);
+
+        // 6. 接收消息，进行消费💥
+        DefaultConsumer defaultConsumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag,
+                                       Envelope envelope,
+                                       AMQP.BasicProperties properties,
+                                       byte[] body) throws IOException
+            {
+                System.out.println("接收到消息: " + new String(body));
+            }
+        };
+        channel.basicConsume("test", true, defaultConsumer);
+    }
+}
---
+++ 25_8_17_rabbitmq_introduction/src/main/java/com/liren/producer.java
@@ -0,0 +1,40 @@
+package com.liren;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+public class producer {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 1. 创建连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+
+        // 2. 设置参数
+        factory.setHost("113.45.137.183");   // 设置ip
+        factory.setPort(5672);               // 设置端口
+        factory.setVirtualHost("lirendada"); // 设置虚拟主机名称
+        factory.setUsername("admin");        // 设置用户名
+        factory.setPassword("123123");       // 设置密码
+
+        // 3. 创建连接Connection
+        Connection connection = factory.newConnection();
+
+        // 4. 通过连接Connection创建通道Channel
+        Channel channel = connection.createChannel();
+
+        // 5. 声明一个队列
+        channel.queueDeclare("test", true, false, false, null);
+
+        // 6. 发送消息（当使用内置交换机的时候，routingKey必须和队列名称保持一致）
+        String text = "hello lirendada!";
+        channel.basicPublish("", "test", null, text.getBytes(StandardCharsets.UTF_8));
+        System.out.println(text + "消息发送成功");
+
+        // 7. 释放资源
+        connection.close();
+    }
+}
