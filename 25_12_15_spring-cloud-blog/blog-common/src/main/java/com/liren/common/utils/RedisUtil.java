6d077bb563807f3c21519c281d86b1538390daec
lirendada <2916776007@qq.com>
2025-12-17 10:58:35 +0800

    引入redis工具类

---
+++ 25_12_15_spring-cloud-blog/blog-common/src/main/java/com/liren/common/utils/RedisUtil.java
@@ -0,0 +1,55 @@
+package com.liren.common.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Slf4j
+public class RedisUtil {
+    private final StringRedisTemplate redisTemplate;
+
+    public RedisUtil(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public boolean set(String key, String value) {
+        try {
+            redisTemplate.opsForValue().set(key, value);
+            return true;
+        } catch (Exception e) {
+            log.error("set redis key: {} value: {} error", key, value, e);
+            return false;
+        }
+    }
+
+    public boolean set(String key, String value, long timeout) {
+        try {
+            if(timeout > 0) {
+                redisTemplate.opsForValue().set(key, value, timeout);
+            } else {
+                set(key, value);
+            }
+            return true;
+        } catch (Exception e) {
+            log.error("set redis key: {} value: {} timeout: {} error", key, value, timeout, e);
+            return false;
+        }
+    }
+
+    public String get(String key) {
+        try {
+            return key == null ? null : redisTemplate.opsForValue().get(key);
+        } catch (Exception e) {
+            log.error("get redis key: {} error", key, e);
+            return null;
+        }
+    }
+
+    public boolean hasKey(String key) {
+        try {
+            return redisTemplate.hasKey(key);
+        } catch (Exception e) {
+            log.error("hasKey redis key: {} error", key, e);
+            return false;
+        }
+    }
+}
